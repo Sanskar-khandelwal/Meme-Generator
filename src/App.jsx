f48e19dd42d856afb374b66ba79a02fce4b8e839
Sanskar khandelwal <Sanskarkhandelwal336@gmail.com>
2023-01-03 16:09:45 +0530

    added the footer in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import Navbar from './component/Navbar'
-import  Meme from './component/Meme'
-
+import React, { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import Navbar from "./component/Navbar";
+import Meme from "./component/Meme";
+import Footer from "./component/Footer";
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
-     <Meme/>
-     </div>
-  )
+      <Navbar />
+      <Meme />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
